74e7b52a484072c892aa2b30ee4b790b58884da4
白勺啊的 <67247524+a2596519067@users.noreply.github.com>
2024-02-17 00:38:52 +0800

    Update diy-part9.sh

--- diy-part9.sh
+++ diy-part9.sh
@@ -11,21 +11,21 @@
 #=================================================
 
 # 修改默认IP
-sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/10.32.0.1/g' package/base-files/files/bin/config_generate
 # 修改网关
-sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.$((addr_offset++)).1/10.32.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名称
-sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
+#sed -i 's/OpenWrt/Yuos/g' package/base-files/files/bin/config_generate
 
 # 修改默认wifi名称ssid为Xiaoyu-Wifi
-sed -i 's/ssid=OpenWrt/ssid=Xiaomi-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/ssid=OpenWrt/ssid=Xiaomi-Wifi/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 # 修改默认wifi密码key为1234567890
-sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i 's/encryption=none/encryption=psk2/g' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 #使用sed 在第四行后添加新字
-sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
+#sed -i '/set wireless.default_radio${devidx}.encryption=psk2/a\set wireless.default_radio${devidx}.key=1234567890' package/kernel/mac80211/files/lib/wifi/mac80211.sh
 
 
 # Add kernel build user
@@ -65,12 +65,12 @@ cp -rf $GITHUB_WORKSPACE/patchs/5.4/shortcut-fe package/kernel/shortcut-fe
 # cp -rf $GITHUB_WORKSPACE/patchs/5.4/network/utils package/network/utils
 
 # 测试编译时间
-YUOS_DATE="$(date +%Y.%m.%d)(新春贺岁版)"
-BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
-echo "Write build date in openwrt : $BUILD_DATE"
-echo -e '\n小渔学长 Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
-sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
-sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
-echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
-sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
+#YUOS_DATE="$(date +%Y.%m.%d)(新春贺岁版)"
+#BUILD_STRING=${BUILD_STRING:-$YUOS_DATE}
+#echo "Write build date in openwrt : $BUILD_DATE"
+#echo -e '\n小渔学长 Build @ '${BUILD_STRING}'\n'  >> package/base-files/files/etc/banner
+#sed -i '/DISTRIB_REVISION/d' package/base-files/files/etc/openwrt_release
+#echo "DISTRIB_REVISION=''" >> package/base-files/files/etc/openwrt_release
+#sed -i '/DISTRIB_DESCRIPTION/d' package/base-files/files/etc/openwrt_release
+#echo "DISTRIB_DESCRIPTION='小渔学长 Build @ ${BUILD_STRING}'" >> package/base-files/files/etc/openwrt_release
+#sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
